f4ecaefaad8d79455ffcbb6df8f6704d8fcc6a62
Takuma Inaba <inataku@MacBook-Pro-3.local>
2017-07-05 04:23:41 +0900

    DIVE19-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'relationships/create'
-
-  get 'relationships/destroy'
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     registrations: "users/registrations",
@@ -30,6 +26,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show]
   resources :relationships, only: [:create, :destroy]
+  resources :conversations do
+    resources :messages
+  end
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
