186e3a57d87c58b53f81e589ee1ea3e1b930c4ae
Takuma Inaba <inataku@MacBook-Pro-3.local>
2017-07-12 00:22:06 +0900

    change storage to fog from file 07120022

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '52.198.63.8', user: 'app', roles: %w{app db web}
+server '13.114.0.75', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/inataku/.ssh/id_rsa'
 
 # server-based syntax
